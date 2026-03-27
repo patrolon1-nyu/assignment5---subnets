1b9ebaa7b9de6872ef456b50e4c58922a8a696e2
Patrick Rolon <pnr7962@nyu.edu>
2026-03-27 18:11:19 -0400

    Added file only to github

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 22224
+
+#problem 1
+p1Network = '176.59.232.240'
+p1Broadcast = '176.59.232.255'
+p1First = '176.59.232.241'
+p1Last =  '176.59.232.254'
+
+#problem 2
+p2Network = '87.44.194.0'
+p2Broadcast = '87.44.194.63'
+p2First = '87.44.194.1'
+p2Last = '87.44.194.62'
+
+#problem 3
+p3Network = '254.29.156.64'
+p3Broadcast = '254.29.156.71'
+p3First = '254.29.156.65'
+p3Last = '254.29.156.70'
+
+#problem 4
+p4Network = '165.14.117.96'
+p4Broadcast = '165.14.117.127'
+p4First = '165.14.117.97'
+p4Last = '165.14.117.126'
+
+#problem 5
+p5Network = '76.255.79.0'
+p5Broadcast = '76.255.79.255'
+p5First = '76.255.79.1'
+p5Last = '76.255.79.254'
+
+#problem 6
+p6Network = '243.240.41.192'
+p6Broadcast = '243.240.41.207'
+p6First = '243.240.41.193'
+p6Last =  '243.240.41.206'
+
+#problem 7
+p7Network = '154.226.3.128'
+p7Broadcast = '154.226.3.255'
+p7First = '154.226.3.129'
+p7Last = '154.226.3.254'
+
+#problem 8
+p8Network = '65.211.220.24'
+p8Broadcast = '65.211.220.31'
+p8First = '65.211.220.25'
+p8Last = '65.211.220.30'
+
+#problem 9
+p9Network = '232.196.182.64'
+p9Broadcast = '232.196.182.127'
+p9First = '232.196.182.65'
+p9Last = '232.196.182.126'
+
+#problem 10
+p10Network = '143.181.144.104'
+p10Broadcast = '143.181.144.111'
+p10First = '143.181.144.105'
+p10Last = '143.181.144.110'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
